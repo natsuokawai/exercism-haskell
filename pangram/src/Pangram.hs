4f659a4987cdbdae205c65b351bb85cd2c7ac6be
natsuokawai <kawai.natsuo@gmail.com>
2020-06-07 12:28:38 +0900

    rewrite alphabet list

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -4,7 +4,7 @@ import qualified Data.Char as C
 import qualified Data.Set as S
 
 alphaList :: String
-alphaList = "abcdefghijklmnopqrstuvwxyz"
+alphaList = ['a'..'z']
 
 isPangram :: String -> Bool
 isPangram text = S.isSubsetOf (S.fromList alphaList) (S.fromList $ map (C.toLower) text)
