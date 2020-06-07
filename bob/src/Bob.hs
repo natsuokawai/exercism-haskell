99f0981e8b34d586313b9a0bbcb1f5f7b2e96850
natsuokawai <kawai.natsuo@gmail.com>
2020-06-07 21:04:14 +0900

    refactor

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -5,32 +5,30 @@ import qualified Data.Text as T
 import Data.Text (Text)
 
 isEmpty :: Text -> Bool
-isEmpty text
-  | text == T.empty = True
-  | otherwise       = False
+isEmpty text = text == T.empty
 
 isQuestion :: Text -> Bool
-isQuestion text
-  | T.last (T.strip text) == '?' = True
-  | otherwise                    = False
+isQuestion text = T.last (T.strip text) == '?'
 
 filterAlpha :: Text -> Text
 filterAlpha text = T.filter C.isAlpha text
 
-isAllUpper :: Text -> Bool
-isAllUpper text
-  | isEmpty text         = False
-  | T.all C.isUpper text = True
-  | otherwise            = False
+allUpper :: Text -> Bool
+allUpper text
+  | isEmpty text = False
+  | otherwise    = T.all C.isUpper text
+
+allNotAlphaNum :: Text -> Bool
+allNotAlphaNum text = T.all (not.C.isAlphaNum) text
 
 responseFor :: String -> String
 responseFor xs 
-  | isEmpty text                     = "Fine. Be that way!"
+  | isEmpty text                   = "Fine. Be that way!"
   | isQuestion text
-    && isAllUpper (filterAlpha text) = "Calm down, I know what I'm doing!"
-  | isQuestion text                  = "Sure."
-  | T.all (not.C.isAlphaNum) text    = "Fine. Be that way!"
-  | isAllUpper (filterAlpha text)    = "Whoa, chill out!"
-  | otherwise                        = "Whatever."
+    && allUpper (filterAlpha text) = "Calm down, I know what I'm doing!"
+  | isQuestion text                = "Sure."
+  | allNotAlphaNum text            = "Fine. Be that way!"
+  | allUpper (filterAlpha text)    = "Whoa, chill out!"
+  | otherwise                      = "Whatever."
   where text = T.strip $ T.pack xs
 
