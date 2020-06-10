7fbe1be042c250e7cbd09e14ad97768af7f2e8f2
natsuokawai <kawai.natsuo@gmail.com>
2020-06-10 22:45:57 +0900

    refactor

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -8,9 +8,10 @@ isEmpty :: Text -> Bool
 isEmpty text = text == T.empty
 
 isQuestion :: Text -> Bool
-isQuestion text
-  | isEmpty text = False
-  | otherwise    = T.last (T.strip text) == '?'
+isQuestion text = T.last (T.strip text) == '?'
+
+isYelling :: Text -> Bool
+isYelling text = allUpper (filterAlpha text)
 
 filterAlpha :: Text -> Text
 filterAlpha = T.filter C.isAlpha
@@ -20,16 +21,20 @@ allUpper text
   | isEmpty text = False
   | otherwise    = T.all C.isUpper text
 
-allNotAlphaNum :: Text -> Bool
-allNotAlphaNum = T.all (not.C.isAlphaNum)
+isAllUpperQuestion :: Text -> Bool
+isAllUpperQuestion text = isQuestion text && isYelling text
+
+isSayingNothing :: Text -> Bool
+isSayingNothing text
+  | isEmpty text = True
+  | otherwise    = (not.isQuestion) text && T.all (not.C.isAlphaNum) text
 
 responseFor :: String -> String
 responseFor xs 
-  | isQuestion text                = if allUpper (filterAlpha text)
-                                       then "Calm down, I know what I'm doing!"
-                                       else "Sure."
-  | allNotAlphaNum text            = "Fine. Be that way!"
-  | allUpper (filterAlpha text)    = "Whoa, chill out!"
-  | otherwise                      = "Whatever."
+  | isSayingNothing text    = "Fine. Be that way!"
+  | isAllUpperQuestion text = "Calm down, I know what I'm doing!"
+  | isQuestion text         = "Sure."
+  | isYelling text          = "Whoa, chill out!"
+  | otherwise               = "Whatever."
   where text = T.strip $ T.pack xs
 
