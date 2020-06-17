86157997729dc35de222881290124de87bea5d31
natsuokawai <kawai.natsuo@gmail.com>
2020-06-17 20:12:57 +0900

    refactor

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,13 +1,15 @@
 module CollatzConjecture (collatz) where
 
-collatz :: Integer -> Maybe Integer
-collatz num
-  | num <= 0  = Nothing
-  | otherwise = Just $ fromIntegral $ length $ collatzList num
+type Count = Integer
 
-collatzList :: Integer -> [Integer]
-collatzList x
-  | x == 1 = []
-  | odd x  = x:collatzList (3 * x + 1)
-  | even x = x:collatzList (x `div` 2)
-collatzList _ = error "Invalid argument."
+collatz :: Integer -> Maybe Count 
+collatz num
+  | num < 1   = Nothing
+  | otherwise = Just $ subCollatz 0 num
+
+subCollatz :: Count -> Integer -> Count
+subCollatz cnt 1 = cnt
+subCollatz cnt num
+  | odd num      = subCollatz (cnt + 1) (3 * num + 1)
+  | even num     = subCollatz (cnt + 1) (num `div` 2)
+subCollatz _ _   = error "Invalid argument"
