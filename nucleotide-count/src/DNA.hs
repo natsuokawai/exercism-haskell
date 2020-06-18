aeeab4334cff0edd776b9e50d44384eacfb1cb31
natsuokawai <kawai.natsuo@gmail.com>
2020-06-19 00:00:42 +0900

    refactor nucleotideCounts

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -4,7 +4,15 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.List as List
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
+toNucleotide :: Char -> Either String Nucleotide
+toNucleotide n
+  | n `elem` "ACGT" = Right ( read [n] :: Nucleotide)
+  | otherwise       = Left ("invalid char: " ++ [n])
+  
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = Map.fromList $ zip [A, C, G, T] (map length (List.group $ List.sort xs))
+nucleotideCounts xs = fmap Map.fromList $ pure zip
+                      <*> (mapM toNucleotide $ List.nub sorted)
+                      <*> pure (map length (List.group sorted))
+                      where sorted = List.sort xs
