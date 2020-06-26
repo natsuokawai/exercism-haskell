9d439ca4a1a6ef62dd2916657f0f99bf60316a16
natsuokawai <kawai.natsuo@gmail.com>
2020-06-26 23:36:33 +0900

    rna transcription

---
+++ rna-transcription/src/DNA.hs
@@ -0,0 +1,12 @@
+module DNA (toRNA) where
+
+dnaComplement :: Char -> Either Char Char
+dnaComplement 'G' = Right 'C'
+dnaComplement 'C' = Right 'G'
+dnaComplement 'T' = Right 'A'
+dnaComplement 'A' = Right 'U'
+dnaComplement  x  = Left x
+
+toRNA :: String -> Either Char String
+toRNA xs = mapM dnaComplement xs
+
