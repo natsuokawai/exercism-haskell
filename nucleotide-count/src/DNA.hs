c6f245da0188290b459480c535a8dcea612b321a
natsuokawai <kawai.natsuo@gmail.com>
2020-06-27 02:22:47 +0900

    modify countUp using map adjust

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -2,7 +2,7 @@ module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import qualified Control.Monad
+import Control.Monad
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
@@ -11,16 +11,12 @@ toNucleotide n
   | n `elem` "ACGT" = Right ( read [n] :: Nucleotide)
   | otherwise       = Left ("invalid char: " ++ [n])
   
-initialCountList :: [(Nucleotide, Int)]
-initialCountList = [(A,0),(C,0),(G,0),(T,0)]
+initialCountList :: Map Nucleotide Int
+initialCountList = Map.fromList [(A,0),(C,0),(G,0),(T,0)]
 
-countUp :: [(Nucleotide, Int)] -> Char -> Either String [(Nucleotide, Int)]
-countUp countList n = case toNucleotide n of
-                        Left message -> Left message
-                        Right x -> Right $ map (inc x) countList
-                        where inc = \n' (nuc, count) -> if n' == nuc
-                                                         then (nuc, count + 1)
-                                                         else (nuc, count)
+countUp :: Map Nucleotide Int -> Char -> Either String (Map Nucleotide Int)
+countUp countList n = pure (Map.adjust (+1)) <*> (toNucleotide n) <*> Right countList
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts n = fmap Map.fromList $ foldM countUp initialCountList n
+nucleotideCounts xs = foldM countUp initialCountList xs
+
