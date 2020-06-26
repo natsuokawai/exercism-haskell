9b2904da4242bf069b1b32a11e731850f1607752
natsuokawai <kawai.natsuo@gmail.com>
2020-06-26 23:24:35 +0900

    impletement toNucleotide using foldM

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -2,7 +2,7 @@ module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import qualified Data.List as List
+import qualified Control.Monad
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
@@ -11,8 +11,16 @@ toNucleotide n
   | n `elem` "ACGT" = Right ( read [n] :: Nucleotide)
   | otherwise       = Left ("invalid char: " ++ [n])
   
+initialCountList :: [(Nucleotide, Int)]
+initialCountList = [(A,0),(C,0),(G,0),(T,0)]
+
+countUp :: [(Nucleotide, Int)] -> Char -> Either String [(Nucleotide, Int)]
+countUp countList n = case toNucleotide n of
+                        Left message -> Left message
+                        Right x -> Right $ map (inc x) countList
+                        where inc = \n' (nuc, count) -> if n' == nuc
+                                                         then (nuc, count + 1)
+                                                         else (nuc, count)
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = fmap Map.fromList $ pure zip
-                      <*> (mapM toNucleotide $ List.nub sorted)
-                      <*> pure (map length (List.group sorted))
-                      where sorted = List.sort xs
+nucleotideCounts n = fmap Map.fromList $ foldM countUp initialCountList n
